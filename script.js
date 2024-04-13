29ceee3c5a9dbf6cac43706040ff74317f9288ed
Nishant Chauhan <mailxnishant@gmail.com>
2024-04-14 04:36:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+function redirectToHome() {
+    window.location.href = "index.html";
+};
+
+function redirectToCommunity() {
+    window.location.href = "community.html";
+};
+
+function redirectToEvents() {
+    window.location.href = "events.html";
+};
+
+function redirectToDietTracker() {
+    window.location.href = "diet.html";
+};
+
+function redirectToBytes() {
+    window.location.href = "bytes.html";
+};
+
+function redirectToExperts() {
+    window.location.href = "experts.html";
+};
+
+document.getElementById('fadeButton').addEventListener('mouseover', function() {
+    this.classList.add('active');
+});
+
+document.getElementById('fadeButton').addEventListener('mouseout', function() {
+    this.classList.remove('active');
+});
+
+document.getElementById("bmi").addEventListener("click", function() {
+    var promptValue = prompt("Enter Weight (in kg):");
+    if (promptValue !== null) {
+        prompt("Enter Height (in cms)");
+    }
+});
+
